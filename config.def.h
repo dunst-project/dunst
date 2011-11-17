b42153d1589f10dc11bca02c0251d7ed394c1e88
Sascha Kruse <knopwob@googlemail.com>
2011-11-17 18:41:59 +0100

    restore old defaults in config.h

--- config.def.h
+++ config.def.h
@@ -16,13 +16,16 @@ const char *critbgcolor = "#ffaaaa";
 const char *critfgcolor = "#000000";
 const char *lowbgcolor =  "#aaaaff";
 const char *lowfgcolor = "#000000";
-const char *format = "%a-->%s %b"; /* default format */
+const char *format = "%s %b"; /* default format */
 int timeouts[] = { 10, 10, 0 }; /* low, normal, critical */
-const char *geom = "0x4-10+10"; /* geometry */
+const char *geom = "0x3-30+20"; /* geometry */
+/* const char *geom = "x1"; */
 
 
-char *key_string = "space"; /* set to NULL for no keybinging */
-KeySym mask = ControlMask;
+char *key_string = NULL; /* set to NULL for no keybinging */
+/* char *key_string = "space"; */
+KeySym mask = 0;
+/* KeySym mask = ControlMask; */
 /* KeySym mask = ControlMask || Mod4Mask */
 
 /* 0 -> print nothing
@@ -35,12 +38,13 @@ int verbosity = 0;
 /* You can use shell-like wildcards to match <appname> <summary> <body> and <icon>. */
 const rule_t rules[] = {
     /* appname,       summary,         body,  icon,  timeout,  urgency,  fg,    bg, format */
-    { "notify-send",  NULL,            NULL,  NULL,  -1,       -1,       NULL,  NULL, "%s %b" },
-    { "Pidgin",       NULL,            NULL,  NULL,  -1,       -1,       NULL,  NULL, "%s %b" },
-    { "Pidgin",       "*signed on*",   NULL,  NULL,  -1,       LOW,      NULL,  NULL, "%s %b" },
-    { "Pidgin",       "*signed off*",  NULL,  NULL,  -1,       LOW,      NULL,  NULL, "%s %b" },
-    { "Pidgin",       "*says*",        NULL,  NULL,  -1,       CRIT,     NULL,  NULL, "%s %b" },
-    { "Pidgin",       "twitter.com*",  NULL,  NULL,  -1,       NORM,     NULL,  NULL, "%s %b" },
+    { NULL,           NULL,            NULL,  NULL,  -1,       -1,       NULL,  NULL, "%s %b" },
+    /* { "notify-send",  NULL,            NULL,  NULL,  -1,       -1,       NULL,  NULL, "%s %b" }, */
+    /* { "Pidgin",       NULL,            NULL,  NULL,  -1,       -1,       NULL,  NULL, "%s %b" }, */
+    /* { "Pidgin",       "*signed on*",   NULL,  NULL,  -1,       LOW,      NULL,  NULL, "%s %b" }, */
+    /* { "Pidgin",       "*signed off*",  NULL,  NULL,  -1,       LOW,      NULL,  NULL, "%s %b" }, */
+    /* { "Pidgin",       "*says*",        NULL,  NULL,  -1,       CRIT,     NULL,  NULL, "%s %b" }, */
+    /* { "Pidgin",       "twitter.com*",  NULL,  NULL,  -1,       NORM,     NULL,  NULL, "%s %b" }, */
 };
 
 
