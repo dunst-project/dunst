ffac73a29ed526ac79b5384a5121d0f3fe7f0ea1
Sascha Kruse <knopwob@googlemail.com>
2012-06-20 17:02:33 +0200

    add license for inih

--- ini.c
+++ ini.c
@@ -1,6 +1,6 @@
 /* inih -- simple .INI file parser
 
-inih is released under the New BSD license (see LICENSE.txt). Go to the project
+inih is released under the New BSD license (see LICENSE.inih.txt). Go to the project
 home page for more info:
 
 http://code.google.com/p/inih/
