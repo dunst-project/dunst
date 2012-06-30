dunst-project/dunst

commit 78b6e2b19f1595b4c6ad38225f667a24d522a576

use libxdg-basedir to properly handle the xdg spec

dunst.c
@@ -16,6 +16,8 @@
 #include <X11/extensions/Xinerama.h>
 #endif
 #include <X11/extensions/scrnsaver.h>
+#include <basedir.h>
+#include <basedir_fs.h>
 
 #include "dunst.h"
 #include "draw.h"
@@ -1056,32 +1058,26 @@ dunst_ini_handle(void *user_data, const char *section,
 void
 parse_dunstrc(void) {
 
-    char *config_path = NULL;
+    xdgHandle xdg;
+    FILE *config_file;
 
     dunst_printf(DEBUG, "Begin parsing of dunstrc\n");
 
+    xdgInitHandle(&xdg);
+
+    config_file = xdgConfigOpen("dunst/dunstrc", "r", &xdg);
     if (config_file == NULL) {
-        config_file = malloc(sizeof(char) * BUFSIZ);
-        memset(config_file, '\0', BUFSIZ);
-
-        config_path = getenv("XDG_CONFIG_HOME");
-
-        if (!config_path) {
-            puts("no dunstrc found -> skipping\n");
-            return;
-        }
-
-
-        strcat(config_file, config_path);
-        strcat(config_file, "/");
-        strcat(config_file, "dunstrc");
-    }
-
-    dunst_printf(DEBUG, "Reading %s\n", config_file);
-
-    if (ini_parse(config_file, dunst_ini_handle, NULL) < 0) {
         puts("no dunstrc found -> skipping\n");
+        xdgWipeHandle(&xdg);
+        return;
     }
+
+    if (ini_parse_file(config_file, dunst_ini_handle, NULL) < 0) {
+        puts("dunstrc could not be parsed -> skipping\n");
+    }
+
+    fclose(config_file);
+    xdgWipeHandle(&xdg);
 
     print_rules();
 }

README.pod
@@ -131,8 +131,8 @@ In order to do that you have to add a hint via the -h option.
 =head1 CONFIGURATION
 
 An example configuration file is included (usually /usr/share/dunst/dunstrc).
-To change the configuration, copy this file to ~/.config/dunstrc and edit it
-accordingly.
+To change the configuration, copy this file to ~/.config/dunst/dunstrc and edit
+it accordingly.
 
 =head1 AUTHOR
 

config.mk
@@ -17,8 +17,8 @@ XINERAMAFLAGS = -DXINERAMA
 INIFLAGS = -DINI_ALLOW_MULTILINE=0
 
 # includes and libs
-INCS = -I${X11INC} $(shell pkg-config --cflags dbus-1) ${XFTINC}
-LIBS = -L${X11LIB} -lX11 -lXss ${XFTLIBS} ${XINERAMALIBS} $(shell pkg-config --libs dbus-1)
+INCS = -I${X11INC} $(shell pkg-config --cflags dbus-1 libxdg-basedir) ${XFTINC}
+LIBS = -L${X11LIB} -lX11 -lXss ${XFTLIBS} ${XINERAMALIBS} $(shell pkg-config --libs dbus-1 libxdg-basedir)
 
 # flags
 CPPFLAGS = -D_BSD_SOURCE -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS} ${INIFLAGS}

commit 45a7ae8a548f347e45888fa1db0db89f9d468a39

fix -config cmdline option
This fixes a regression that got introduced with
commit 78b6e2b19f1595b4c6ad38225f667a24d522a576

dunst.c
@@ -89,7 +89,6 @@ static screen_info scr;
 static dimension_t geometry;
 static XScreenSaverInfo *screensaver_info;
 static int font_h;
-static char *config_file;
 
 int next_notification_id = 1;
 
@@ -1190,16 +1189,21 @@ dunst_ini_handle(void *user_data, const char *section,
         return 1;
 }
 
-void parse_dunstrc(void)
+void parse_dunstrc(char *cmdline_config_path)
 {
 
         xdgHandle xdg;
-        FILE *config_file;
+        FILE *config_file = NULL;
 
         xdgInitHandle(&xdg);
         rules = l_init();
 
-        config_file = xdgConfigOpen("dunst/dunstrc", "r", &xdg);
+        if (cmdline_config_path != NULL) {
+                config_file = fopen(cmdline_config_path, "r");
+        }
+        if (config_file == NULL) {
+                config_file = xdgConfigOpen("dunst/dunstrc", "r", &xdg);
+        }
         if (config_file == NULL) {
                 /* Fall back to just "dunstrc", which was used before 2012-06-23
                  * (before v0.2). */
@@ -1221,7 +1225,7 @@ void parse_dunstrc(void)
         print_rules();
 }
 
-void parse_cmdline_for_config_file(int argc, char *argv[])
+char *parse_cmdline_for_config_file(int argc, char *argv[])
 {
         int i;
         for (i = 0; i < argc; i++) {
@@ -1230,21 +1234,22 @@ void parse_cmdline_for_config_file(int argc, char *argv[])
                                 printf
                                     ("Invalid commandline: -config needs argument\n");
                         }
-                        config_file = argv[++i];
-                        return;
+                        return argv[++i];
                 }
         }
+        return NULL;
 }
 
 int main(int argc, char *argv[])
 {
+        char *cmdline_config_path;
         now = time(&now);
         geometry.mask = XParseGeometry(geom,
                                        &geometry.x, &geometry.y,
                                        &geometry.w, &geometry.h);
 
-        parse_cmdline_for_config_file(argc, argv);
-        parse_dunstrc();
+        cmdline_config_path = parse_cmdline_for_config_file(argc, argv);
+        parse_dunstrc(cmdline_config_path);
         parse_cmdline(argc, argv);
         dc = initdc();
 