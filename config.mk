14d59c4d0c89d4ab8b5e74124c895a5d7841a25f
Sascha Kruse <knopwob@googlemail.com>
2013-03-20 08:54:44 +0000

    only add libxdg-basedir as dependency when needed

--- config.mk
+++ config.mk
@@ -25,17 +25,22 @@ ifeq (${PKG_CONFIG}, ${EMPTY})
 	$(error "Failed to find pkg-config, please make sure it is installed")
 endif
 
-pkg_config_packs:="dbus-1 libxdg-basedir x11 freetype2 xext xft xscrnsaver glib-2.0 gio-2.0 pango cairo pangocairo"
+# flags
+CPPFLAGS += -D_BSD_SOURCE -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS} ${INIFLAGS}
+CFLAGS   += -g --std=c99 -pedantic -Wall -Wno-overlength-strings -Os ${STATIC} ${CPPFLAGS} ${EXTRACFLAGS}
+
+pkg_config_packs:="dbus-1 x11 freetype2 xext xft xscrnsaver glib-2.0 gio-2.0 pango cairo pangocairo"
+
+# check if we need libxdg-basedir
+ifeq (,$(findstring STATIC_CONFIG,$(CFLAGS)))
+	pkg_config_packs += libxdg-basedir
+endif
 
 # includes and libs
 INCS := $(shell ${PKG_CONFIG} --cflags ${pkg_config_packs})
-LIBS := -lm -L${X11LIB} -lXss ${XINERAMALIBS} $(shell ${PKG_CONFIG} --libs ${pkg_config_packs})
+CFLAGS += ${INCS}
+LDFLAGS += -lm -L${X11LIB} -lXss ${XINERAMALIBS} $(shell ${PKG_CONFIG} --libs ${pkg_config_packs})
 
 ifeq (${INCS}, ${EMPTY})
     $(error "pkg-config failed, see errors above")
 endif
-
-# flags
-CPPFLAGS += -D_BSD_SOURCE -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS} ${INIFLAGS}
-CFLAGS   += -g --std=c99 -pedantic -Wall -Wno-overlength-strings -Os ${INCS} ${STATIC} ${CPPFLAGS} ${EXTRACFLAGS}
-LDFLAGS  += ${LIBS}
