11aef25bd573b111be743de855bc99750701ab72
Benedikt Heine <bebe@bebehei.de>
2017-10-07 12:08:24 +0200

    Change default icons to match freedesktop spec

--- config.def.h
+++ config.def.h
@@ -11,7 +11,7 @@ char *lowfgcolor = "#000000";
 char *format = "%s %b";         /* default format */
 
 int timeouts[] = { 10, 10, 0 }; /* low, normal, critical */
-char *icons[] = { "info", "info", "emblem-important" }; /* low, normal, critical */
+char *icons[] = { "dialog-information", "dialog-information", "dialog-warning" }; /* low, normal, critical */
 
 unsigned int transparency = 0;  /* transparency */
 char *geom = "0x0";             /* geometry */
