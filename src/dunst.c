2d7dc6fbc592bf7a764286d8d8d42fda7db7549b
Benedikt Heine <bebe@bebehei.de>
2017-10-11 16:55:28 +0200

    Make startup notification timeout in 10 seconds

--- src/dunst.c
+++ src/dunst.c
@@ -316,7 +316,7 @@ int dunst_main(int argc, char *argv[])
                 n->summary = g_strdup("startup");
                 n->body = g_strdup("dunst is up and running");
                 n->progress = 0;
-                n->timeout = 10;
+                n->timeout = 10 * G_USEC_PER_SEC;
                 n->markup = MARKUP_NO;
                 n->urgency = LOW;
                 notification_init(n, 0);
