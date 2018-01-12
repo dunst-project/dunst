877eaa0b5d90bfbb0afb4cfb16a1e4f40f7849b9
Benedikt Heine <bebe@bebehei.de>
2018-01-12 19:57:33 +0100

    Remove colors field from xctx
    The xctx color field is a full duplicate of the settings logic.
    Only logic included in xctx color fields, are the frame colors, which
    fall back to the global frame setting. So, this required to handle it
    directly in settings.c

--- src/notification.c
+++ src/notification.c
@@ -21,7 +21,6 @@
 #include "rules.h"
 #include "settings.h"
 #include "utils.h"
-#include "x11/x.h"
 
 static void notification_extract_urls(notification *n);
 static void notification_format_message(notification *n);
@@ -316,12 +315,26 @@ void notification_init(notification *n)
                 n->icon = g_strdup(settings.icons[n->urgency]);
 
         /* Color hints */
+        NotificationColors defcolors;
+        switch (n->urgency) {
+                case URG_LOW:
+                        defcolors = settings.colors_low;
+                        break;
+                case URG_NORM:
+                        defcolors = settings.colors_norm;
+                        break;
+                case URG_CRIT:
+                        defcolors = settings.colors_crit;
+                        break;
+                default:
+                        g_error("Unhandled urgency type: %d", n->urgency);
+        }
         if (!n->colors.fg)
-                n->colors.fg = g_strdup(xctx.colors[ColFG][n->urgency]);
+                n->colors.fg = g_strdup(defcolors.fg);
         if (!n->colors.bg)
-                n->colors.bg = g_strdup(xctx.colors[ColBG][n->urgency]);
+                n->colors.bg = g_strdup(defcolors.bg);
         if (!n->colors.frame)
-                n->colors.frame = g_strdup(xctx.colors[ColFrame][n->urgency]);
+                n->colors.frame = g_strdup(defcolors.frame);
 
         /* Sanitize misc hints */
         if (n->progress < 0)
--- src/settings.c
+++ src/settings.c
@@ -499,7 +499,7 @@ void load_settings(char *cmdline_config_path)
 
         settings.colors_low.frame = option_get_string(
                 "urgency_low",
-                "frame_color", "-lfr", NULL,
+                "frame_color", "-lfr", settings.frame_color ? settings.frame_color : defaults.colors_low.frame,
                 "Frame color for notifications with low urgency"
         );
 
@@ -529,7 +529,7 @@ void load_settings(char *cmdline_config_path)
 
         settings.colors_norm.frame = option_get_string(
                 "urgency_normal",
-                "frame_color", "-nfr", NULL,
+                "frame_color", "-nfr", settings.frame_color ? settings.frame_color : defaults.colors_norm.frame,
                 "Frame color for notifications with normal urgency"
         );
 
@@ -559,7 +559,7 @@ void load_settings(char *cmdline_config_path)
 
         settings.colors_crit.frame = option_get_string(
                 "urgency_critical",
-                "frame_color", "-cfr", NULL,
+                "frame_color", "-cfr", settings.frame_color ? settings.frame_color : defaults.colors_crit.frame,
                 "Frame color for notifications with critical urgency"
         );
 
