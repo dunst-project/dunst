dunst-project/dunst

commit 16e38a9a84c46b0cd4fd7b0f2f88e9d8054ccc0b

Don't draw frame with zero line width

src/draw.c
@@ -464,23 +464,26 @@ static cairo_surface_t *render_background(cairo_surface_t *srf,
         else
                 height += settings.separator_height;
 
-        cairo_set_source_rgb(c, cl->frame.r, cl->frame.g, cl->frame.b);
-        draw_rounded_rect(c, x, y, width, height, corner_radius, first, last);
-        cairo_fill(c);
+	if (settings.frame_width > 0)
+        {
+            cairo_set_source_rgb(c, cl->frame.r, cl->frame.g, cl->frame.b);
+            draw_rounded_rect(c, x, y, width, height, corner_radius, first, last);
+            cairo_fill(c);
 
-        /* adding frame */
-        x += settings.frame_width;
-        if (first) {
-                y += settings.frame_width;
-                height -= settings.frame_width;
+            /* adding frame */
+            x += settings.frame_width;
+            if (first) {
+                    y += settings.frame_width;
+                    height -= settings.frame_width;
+            }
+
+            width -= 2 * settings.frame_width;
+
+            if (last)
+                    height -= settings.frame_width;
+            else
+                    height -= settings.separator_height;
         }
-
-        width -= 2 * settings.frame_width;
-
-        if (last)
-                height -= settings.frame_width;
-        else
-                height -= settings.separator_height;
 
         cairo_set_source_rgb(c, cl->bg.r, cl->bg.g, cl->bg.b);
         draw_rounded_rect(c, x, y, width, height, corner_radius, first, last);

commit 7735c9a4f60ef8cd645a39a98557c8950d0fa9ec

Fix separator not working with frame_width=0
Bug was introduced with 16e38a9a84c46b0cd4fd7b0f2f88e9d8054ccc0b.

src/draw.c
@@ -510,29 +510,27 @@ static cairo_surface_t *render_background(cairo_surface_t *srf,
         else
                 height += settings.separator_height;
 
-        if (settings.frame_width > 0) {
-                draw_rounded_rect(c, x, y, width, height, corner_radius, first, last);
+        draw_rounded_rect(c, x, y, width, height, corner_radius, first, last);
 
-                /* adding frame */
-                x += settings.frame_width;
-                if (first) {
-                        y += settings.frame_width;
-                        height -= settings.frame_width;
-                }
-
-                width -= 2 * settings.frame_width;
-
-                if (last)
-                        height -= settings.frame_width;
-                else
-                        height -= settings.separator_height;
-
-                radius_int = frame_internal_radius (corner_radius, settings.frame_width, height);
-
-                draw_rounded_rect(c, x, y, width, height, radius_int, first, last);
-                cairo_set_source_rgba(c, cl->frame.r, cl->frame.g, cl->frame.b, cl->frame.a);
-                cairo_fill(c);
+        /* adding frame */
+        x += settings.frame_width;
+        if (first) {
+                y += settings.frame_width;
+                height -= settings.frame_width;
         }
+
+        width -= 2 * settings.frame_width;
+
+        if (last)
+                height -= settings.frame_width;
+        else
+                height -= settings.separator_height;
+
+        radius_int = frame_internal_radius (corner_radius, settings.frame_width, height);
+
+        draw_rounded_rect(c, x, y, width, height, radius_int, first, last);
+        cairo_set_source_rgba(c, cl->frame.r, cl->frame.g, cl->frame.b, cl->frame.a);
+        cairo_fill(c);
 
         draw_rounded_rect(c, x, y, width, height, radius_int, first, last);
         cairo_set_source_rgba(c, cl->bg.r, cl->bg.g, cl->bg.b, cl->bg.a);