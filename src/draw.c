acf59ad9f22c1aff327e0e73a21dc94e74882c59
fwsmit <fw.smit01@gmail.com>
2021-04-04 12:53:28 +0200

    settings: Implement origin and offset

--- src/draw.c
+++ src/draw.c
@@ -780,22 +780,49 @@ static struct dimensions layout_render(cairo_surface_t *srf,
  */
 static void calc_window_pos(int width, int height, int *ret_x, int *ret_y)
 {
+        if(!ret_x || !ret_y)
+                return;
+
         const struct screen_info *scr = output->get_active_screen();
 
-        if (ret_x) {
-                if (settings.geometry.negative_x) {
-                        *ret_x = (scr->x + (scr->w - width)) + settings.geometry.x;
-                } else {
-                        *ret_x = scr->x + settings.geometry.x;
-                }
+        // horizontal
+        switch (settings.origin) {
+                case ORIGIN_TOP_LEFT:
+                case ORIGIN_LEFT_CENTER:
+                case ORIGIN_BOTTOM_LEFT:
+                        *ret_x = scr->x + settings.offset.x;
+                        break;
+                case ORIGIN_TOP_RIGHT:
+                case ORIGIN_RIGHT_CENTER:
+                case ORIGIN_BOTTOM_RIGHT:
+                        *ret_x = scr->x + (scr->w - width) - settings.offset.x;
+                        break;
+                case ORIGIN_TOP_CENTER:
+                case ORIGIN_CENTER:
+                case ORIGIN_BOTTOM_CENTER:
+                default:
+                        *ret_x = scr->x + (scr->w - width) / 2;
+                        break;
         }
 
-        if (ret_y) {
-                if (settings.geometry.negative_y) {
-                        *ret_y = scr->y + (scr->h + settings.geometry.y) - height;
-                } else {
-                        *ret_y = scr->y + settings.geometry.y;
-                }
+        // vertical
+        switch (settings.origin) {
+                case ORIGIN_TOP_LEFT:
+                case ORIGIN_TOP_CENTER:
+                case ORIGIN_TOP_RIGHT:
+                        *ret_y = scr->y + settings.offset.y;
+                        break;
+                case ORIGIN_BOTTOM_LEFT:
+                case ORIGIN_BOTTOM_CENTER:
+                case ORIGIN_BOTTOM_RIGHT:
+                        *ret_y = scr->y + (scr->h - height) - settings.offset.y;
+                        break;
+                case ORIGIN_LEFT_CENTER:
+                case ORIGIN_CENTER:
+                case ORIGIN_RIGHT_CENTER:
+                default:
+                        *ret_y = scr->y + (scr->h - height) / 2;
+                        break;
         }
 }
 
