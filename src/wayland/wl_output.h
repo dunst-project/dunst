0671c09ede40357575e0f21f1a7907147dab8fbe
fwsmit <fw.smit01@gmail.com>
2021-10-06 17:43:34 +0200

    wayland: rewrite foreign toplevel support
    This is mostly taken from examples/foreign-toplevel.c of wlroots.

---
+++ src/wayland/wl_output.h
@@ -0,0 +1,20 @@
+#ifndef DUNST_WL_OUTPUT_H
+#define DUNST_WL_OUTPUT_H
+#include <stdint.h>
+#include <stdbool.h>
+#include <wayland-util.h>
+
+struct dunst_output {
+        uint32_t global_name;
+        char *name;
+        struct wl_output *wl_output;
+        struct wl_list link;
+
+        uint32_t scale;
+        uint32_t subpixel; // TODO do something with it
+        bool fullscreen;
+        struct zwlr_foreign_toplevel_handle_v1 *fullscreen_toplevel; // the toplevel that is fullscreened on this output
+};
+
+#endif
+/* vim: set ft=c tabstop=8 shiftwidth=8 expandtab textwidth=0: */
