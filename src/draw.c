dunst-project/dunst

commit 0b1cf92960df3f39bb732056c37af03e6c676df6

icon: convert stored icon from GdkPixbuf to cairo_surface

src/draw.c
@@ -209,7 +209,6 @@ static void free_colored_layout(void *data)
         g_object_unref(cl->l);
         pango_attr_list_unref(cl->attr);
         g_free(cl->text);
-        if (cl->icon) cairo_surface_destroy(cl->icon);
         g_free(cl);
 }
 
@@ -304,17 +303,7 @@ static struct colored_layout *layout_from_notification(cairo_t *c, struct notifi
 
         struct colored_layout *cl = layout_init_shared(c, n);
 
-        if (settings.icon_position != ICON_OFF && n->icon) {
-                cl->icon = gdk_pixbuf_to_cairo_surface(n->icon);
-        } else {
-                cl->icon = NULL;
-        }
-
-        if (cl->icon && cairo_surface_status(cl->icon) != CAIRO_STATUS_SUCCESS) {
-                cairo_surface_destroy(cl->icon);
-                cl->icon = NULL;
-        }
-
+        cl->icon = n->icon;
         /* markup */
         GError *err = NULL;
         pango_parse_markup(n->text_to_render, -1, 0, &(cl->attr), &(cl->text), NULL, &err);

src/icon.h
@@ -43,34 +43,6 @@ int get_icon_height(cairo_surface_t *icon, double scale);
  */
 char *get_path_from_icon_name(const char *iconname);
 
-/** Retrieve an icon by its name sent via the notification bus, scaled according to settings
- *
- * @param iconname A string describing a `file://` URL, an arbitary filename
- *                 or an icon name, which then gets searched for in the
- *                 settings.icon_path
- * @param scale An integer representing the output dpi scaling.
- *
- * @return an instance of `GdkPixbuf`
- * @retval NULL: file does not exist, not readable, etc..
- */
-GdkPixbuf *get_pixbuf_from_icon(const char *iconname, double scale);
-
-/** Read an icon from disk and convert it to a GdkPixbuf, scaled according to settings
- *
- * The returned id will be a unique identifier. To check if two given
- * GdkPixbufs are equal, it's sufficient to just compare the id strings.
- *
- * @param name A string describing and icon. May be a full path, a file path or
- *             just a simple name. If it's a name without a slash, the icon will
- *             get searched in the folders of the icon_path setting.
- * @param id   (necessary) A unique identifier of the returned pixbuf. Only filled,
- *             if the return value is non-NULL.
- * @param dpi_scale An integer representing the output dpi scaling.
- * @return an instance of `GdkPixbuf`, representing the name's image
- * @retval NULL: Invalid path given
- */
-GdkPixbuf *icon_get_for_name(const char *name, char **id, double dpi_scale);
-
 /** Convert a GVariant like described in GdkPixbuf, scaled according to settings
  *
  * The returned id will be a unique identifier. To check if two given

src/notification.c
@@ -24,6 +24,7 @@
 #include "settings.h"
 #include "utils.h"
 #include "draw.h"
+#include "icon-lookup.h"
 
 static void notification_extract_urls(struct notification *n);
 static void notification_format_message(struct notification *n);
@@ -292,7 +293,7 @@ void notification_unref(struct notification *n)
         g_free(n->default_action_name);
 
         if (n->icon)
-                g_object_unref(n->icon);
+                cairo_surface_destroy(n->icon);
         g_free(n->icon_id);
 
         notification_private_free(n->priv);
@@ -313,10 +314,21 @@ void notification_icon_replace_path(struct notification *n, const char *new_icon
         g_free(n->iconname);
         n->iconname = g_strdup(new_icon);
 
-        g_clear_object(&n->icon);
+        cairo_surface_destroy(n->icon);
+        n->icon = NULL;
         g_clear_pointer(&n->icon_id, g_free);
 
-        n->icon = icon_get_for_name(new_icon, &n->icon_id, draw_get_scale());
+        g_free(n->icon_path);
+        n->icon_path = find_icon_path(new_icon, settings.max_icon_size);
+        if (n->icon_path) {
+                GdkPixbuf *pixbuf = get_pixbuf_from_file(n->icon_path, draw_get_scale());
+                if (pixbuf) {
+                        n->icon = gdk_pixbuf_to_cairo_surface(pixbuf);
+                        g_object_unref(pixbuf);
+                } else {
+                        LOG_W("No icon found in path: '%s'", n->icon_path);
+                }
+        }
 }
 
 void notification_icon_replace_data(struct notification *n, GVariant *new_icon)
@@ -324,10 +336,13 @@ void notification_icon_replace_data(struct notification *n, GVariant *new_icon)
         ASSERT_OR_RET(n,);
         ASSERT_OR_RET(new_icon,);
 
-        g_clear_object(&n->icon);
+        cairo_surface_destroy(n->icon);
+        n->icon = NULL;
         g_clear_pointer(&n->icon_id, g_free);
 
-        n->icon = icon_get_for_data(new_icon, &n->icon_id, draw_get_scale());
+        GdkPixbuf *icon = icon_get_for_data(new_icon, &n->icon_id, draw_get_scale());
+        n->icon = gdk_pixbuf_to_cairo_surface(icon);
+        g_object_unref(icon);
 }
 
 /* see notification.h */

src/notification.h
@@ -2,10 +2,10 @@
 #ifndef DUNST_NOTIFICATION_H
 #define DUNST_NOTIFICATION_H
 
-#include <gdk-pixbuf/gdk-pixbuf.h>
 #include <glib.h>
 #include <stdbool.h>
 #include <pango/pango-layout.h>
+#include <cairo.h>
 
 #include "markup.h"
 
@@ -50,11 +50,12 @@ struct notification {
         char *desktop_entry;     /**< The desktop entry hint sent via every GApplication */
         enum urgency urgency;
 
-        GdkPixbuf *icon;         /**< The raw cached icon data used to draw */
-        char *icon_id;           /**< plain icon information, which acts as the pixbuf's id, which is saved in .icon
+        cairo_surface_t *icon;         /**< The raw cached icon data used to draw */
+        char *icon_id;           /**< Plain icon information, which acts as the icon's id.
                                       May be a hash for a raw icon or a name/path for a regular app icon. */
-        char *iconname;          /**< plain icon information (may be a path or just a name)
-                                      Use this to compare the icon name with rules.*/
+        char *iconname;          /**< plain icon information (may be a path or just a name) as recieved from dbus.
+                                   Use this to compare the icon name with rules. May also be modified by rules.*/
+        char *icon_path;         /**< Full path to the notification's icon. */
 
         gint64 start;      /**< begin of current display (in milliseconds) */
         gint64 timestamp;  /**< arrival time (in milliseconds) */

test/notification.c
@@ -4,6 +4,7 @@
 
 #include "../src/option_parser.h"
 #include "../src/settings.h"
+#include "../src/icon.h"
 
 extern const char *base;
 
@@ -150,8 +151,8 @@ TEST test_notification_icon_scaling_toosmall(void)
 {
         struct notification *n = notification_load_icon_with_scaling(20, 100);
 
-        ASSERT_EQ(gdk_pixbuf_get_width(n->icon), 20);
-        ASSERT_EQ(gdk_pixbuf_get_height(n->icon), 20);
+        ASSERT_EQ(get_icon_width(n->icon, 1), 20);
+        ASSERT_EQ(get_icon_height(n->icon, 1), 20);
 
         notification_unref(n);
 
@@ -163,8 +164,8 @@ TEST test_notification_icon_scaling_toolarge(void)
 {
         struct notification *n = notification_load_icon_with_scaling(5, 10);
 
-        ASSERT_EQ(gdk_pixbuf_get_width(n->icon), 10);
-        ASSERT_EQ(gdk_pixbuf_get_height(n->icon), 10);
+        ASSERT_EQ(get_icon_width(n->icon, 1), 10);
+        ASSERT_EQ(get_icon_height(n->icon, 1), 10);
 
         notification_unref(n);
 
@@ -175,8 +176,8 @@ TEST test_notification_icon_scaling_notconfigured(void)
 {
         struct notification *n = notification_load_icon_with_scaling(0, 0);
 
-        ASSERT_EQ(gdk_pixbuf_get_width(n->icon), 16);
-        ASSERT_EQ(gdk_pixbuf_get_height(n->icon), 16);
+        ASSERT_EQ(get_icon_width(n->icon, 1), 16);
+        ASSERT_EQ(get_icon_height(n->icon, 1), 16);
 
         notification_unref(n);
 
@@ -187,8 +188,8 @@ TEST test_notification_icon_scaling_notneeded(void)
 {
         struct notification *n = notification_load_icon_with_scaling(10, 20);
 
-        ASSERT_EQ(gdk_pixbuf_get_width(n->icon), 16);
-        ASSERT_EQ(gdk_pixbuf_get_height(n->icon), 16);
+        ASSERT_EQ(get_icon_width(n->icon, 1), 16);
+        ASSERT_EQ(get_icon_height(n->icon, 1), 16);
 
         notification_unref(n);
 

commit 9a599b9f3c5d9e0cffcd5958c78902787cf0f428

Fix 'icon_position = off' not being respected
Regression was introduced with commit
0b1cf92960df3f39bb732056c37af03e6c676df6

src/draw.c
@@ -335,7 +335,12 @@ static struct colored_layout *layout_from_notification(cairo_t *c, struct notifi
 
         struct colored_layout *cl = layout_init_shared(c, n);
 
-        cl->icon = n->icon;
+        if (settings.icon_position != ICON_OFF && n->icon) {
+                cl->icon = n->icon;
+        } else {
+                cl->icon = NULL;
+        }
+
         /* markup */
         GError *err = NULL;
         pango_parse_markup(n->text_to_render, -1, 0, &(cl->attr), &(cl->text), NULL, &err);