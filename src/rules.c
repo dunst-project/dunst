0f8819d98ae13df7560badacbd45ea2992f0cd28
Federico Angelilli <code@fedang.net>
2026-01-28 00:27:18 +0100

    Update valgrind suppressions and fix some code

--- src/rules.c
+++ src/rules.c
@@ -274,7 +274,7 @@ void rule_free(struct rule *r)
 static inline bool rule_field_matches_string(const char *value, const char *pattern)
 {
         if (settings.enable_regex) {
-                if (!pattern) {
+                if (STR_EMPTY(pattern)) {
                         return true;
                 }
                 if (!value) {
