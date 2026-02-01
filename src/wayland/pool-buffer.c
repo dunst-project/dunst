e7f7f40becdc19aa2f2a46c93ca2883e20a3d968
Federico Angelilli <code@fedang.net>
2026-02-01 03:10:26 +0100

    Refactor wayland pool buffer

--- src/wayland/pool-buffer.c
+++ src/wayland/pool-buffer.c
@@ -16,141 +16,145 @@
 #include <string.h>
 
 #include "pool-buffer.h"
+#include "../utils.h"
 
-static void randname(char *buf) {
-	struct timespec ts;
-	clock_gettime(CLOCK_REALTIME, &ts);
-	long r = ts.tv_nsec;
-	for (int i = 0; i < 6; ++i) {
-		buf[i] = 'A'+(r&15)+(r&16)*2;
-		r >>= 5;
-	}
+/**
+ * Generate unique-ish name for a shm file,
+ * create it and immediately unlink it.
+ */
+static int anonymous_shm_open(void)
+{
+        char name[] = "/dunst-XXXXXX";
+        int retries = 100;
+        gint64 pid = (gint64)getpid() << 32;
+
+        do {
+                gint64 r = time_now() ^ pid ^ retries;
+                for (int i = 0; i < 6; ++i) {
+                        int num = (r & 15) + (r & 16) * 2;
+                        name[i + sizeof(name) - 7] = 'A' + num;
+                        r >>= 5;
+                }
+
+                --retries;
+
+                // shm_open guarantees that O_CLOEXEC is set
+                int fd = shm_open(name, O_RDWR | O_CREAT | O_EXCL, 0600);
+                if (fd >= 0) {
+                        shm_unlink(name);
+                        return fd;
+                }
+        } while (retries > 0 && errno == EEXIST);
+
+        return -1;
 }
 
-static int anonymous_shm_open(void) {
-	char name[] = "/dunst-XXXXXX";
-	int retries = 100;
+static int create_shm_file(off_t size)
+{
+        int fd = anonymous_shm_open();
+        if (fd < 0) {
+                return fd;
+        }
 
-	do {
-		randname(name + strlen(name) - 6);
+        if (ftruncate(fd, size) < 0) {
+                close(fd);
+                return -1;
+        }
 
-		--retries;
-		// shm_open guarantees that O_CLOEXEC is set
-		int fd = shm_open(name, O_RDWR | O_CREAT | O_EXCL, 0600);
-		if (fd >= 0) {
-			shm_unlink(name);
-			return fd;
-		}
-	} while (retries > 0 && errno == EEXIST);
-
-	return -1;
+        return fd;
 }
 
-static int create_shm_file(off_t size) {
-	int fd = anonymous_shm_open();
-	if (fd < 0) {
-		return fd;
-	}
-
-	if (ftruncate(fd, size) < 0) {
-		close(fd);
-		return -1;
-	}
-
-	return fd;
-}
-
-static void buffer_handle_release(void *data, struct wl_buffer *wl_buffer) {
-	struct pool_buffer *buffer = data;
-	buffer->busy = false;
+static void buffer_handle_release(void *data, struct wl_buffer *wl_buffer)
+{
+        struct pool_buffer *buffer = data;
+        buffer->busy = false;
 }
 
 static const struct wl_buffer_listener buffer_listener = {
-	.release = buffer_handle_release,
+        .release = buffer_handle_release,
 };
 
-static struct pool_buffer *create_buffer(struct wl_shm *shm,
-		struct pool_buffer *buf, int32_t width, int32_t height) {
-	const enum wl_shm_format wl_fmt = WL_SHM_FORMAT_ARGB8888;
-	const cairo_format_t cairo_fmt = CAIRO_FORMAT_ARGB32;
+static struct pool_buffer *create_buffer(struct wl_shm *shm, struct pool_buffer *buf,
+                                         int32_t width, int32_t height)
+{
+        const enum wl_shm_format wl_fmt = WL_SHM_FORMAT_ARGB8888;
+        const cairo_format_t cairo_fmt = CAIRO_FORMAT_ARGB32;
 
-	uint32_t stride = cairo_format_stride_for_width(cairo_fmt, width);
-	size_t size = stride * height;
+        uint32_t stride = cairo_format_stride_for_width(cairo_fmt, width);
+        size_t size = stride * height;
 
-	void *data = NULL;
-	if (size > 0) {
-		int fd = create_shm_file(size);
-		if (fd == -1) {
-			return NULL;
-		}
+        void *data = NULL;
+        if (size > 0) {
+                int fd = create_shm_file(size);
+                if (fd == -1) {
+                        return NULL;
+                }
 
-		data = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-		if (data == MAP_FAILED) {
-			close(fd);
-			return NULL;
-		}
+                data = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+                if (data == MAP_FAILED) {
+                        close(fd);
+                        return NULL;
+                }
 
-		struct wl_shm_pool *pool = wl_shm_create_pool(shm, fd, size);
-		buf->buffer =
-			wl_shm_pool_create_buffer(pool, 0, width, height, stride, wl_fmt);
-		wl_buffer_add_listener(buf->buffer, &buffer_listener, buf);
-		wl_shm_pool_destroy(pool);
+                struct wl_shm_pool *pool = wl_shm_create_pool(shm, fd, size);
+                buf->buffer = wl_shm_pool_create_buffer(pool, 0, width, height, stride, wl_fmt);
+                wl_buffer_add_listener(buf->buffer, &buffer_listener, buf);
+                wl_shm_pool_destroy(pool);
 
-		close(fd);
-	}
+                close(fd);
+        }
 
-	buf->data = data;
-	buf->size = size;
-	buf->width = width;
-	buf->height = height;
-	buf->surface = cairo_image_surface_create_for_data(data, cairo_fmt, width,
-		height, stride);
-	buf->cairo = cairo_create(buf->surface);
-	buf->pango = pango_cairo_create_context(buf->cairo);
-	return buf;
+        buf->data = data;
+        buf->size = size;
+        buf->width = width;
+        buf->height = height;
+        buf->surface = cairo_image_surface_create_for_data(data, cairo_fmt, width, height, stride);
+        buf->cairo = cairo_create(buf->surface);
+        buf->pango = pango_cairo_create_context(buf->cairo);
+        return buf;
 }
 
-void finish_buffer(struct pool_buffer *buffer) {
-	if (buffer->buffer) {
-		wl_buffer_destroy(buffer->buffer);
-	}
-	if (buffer->cairo) {
-		cairo_destroy(buffer->cairo);
-	}
-	if (buffer->surface) {
-		cairo_surface_destroy(buffer->surface);
-	}
-	if (buffer->pango) {
-		g_object_unref(buffer->pango);
-	}
-	if (buffer->data) {
-		munmap(buffer->data, buffer->size);
-	}
-	memset(buffer, 0, sizeof(struct pool_buffer));
+void finish_buffer(struct pool_buffer *buffer)
+{
+        if (buffer->buffer)
+                wl_buffer_destroy(buffer->buffer);
+
+        if (buffer->cairo)
+                cairo_destroy(buffer->cairo);
+
+        if (buffer->surface)
+                cairo_surface_destroy(buffer->surface);
+
+        if (buffer->pango)
+                g_object_unref(buffer->pango);
+
+        if (buffer->data)
+                munmap(buffer->data, buffer->size);
+
+        memset(buffer, 0, sizeof(struct pool_buffer));
 }
 
-struct pool_buffer *get_next_buffer(struct wl_shm *shm,
-		struct pool_buffer pool[static 2], uint32_t width, uint32_t height) {
-	struct pool_buffer *buffer = NULL;
-	for (size_t i = 0; i < 2; ++i) {
-		if (pool[i].busy) {
-			continue;
-		}
-		buffer = &pool[i];
-	}
-	if (!buffer) {
-		return NULL;
-	}
+struct pool_buffer *get_next_buffer(struct wl_shm *shm, struct pool_buffer pool[static 2],
+                                    uint32_t width, uint32_t height)
+{
+        struct pool_buffer *buffer = NULL;
 
-	if (buffer->width != width || buffer->height != height) {
-		finish_buffer(buffer);
-	}
+        for (size_t i = 0; i < 2; ++i) {
+                if (pool[i].busy)
+                        continue;
+                buffer = &pool[i];
+        }
 
-	if (!buffer->buffer) {
-		if (!create_buffer(shm, buffer, width, height)) {
-			return NULL;
-		}
-	}
+        if (!buffer)
+                return NULL;
 
-	return buffer;
+        if (buffer->width != width || buffer->height != height)
+                finish_buffer(buffer);
+
+        if (!buffer->buffer) {
+                if (!create_buffer(shm, buffer, width, height))
+                        return NULL;
+        }
+
+        return buffer;
 }
--- src/wayland/pool-buffer.h
+++ src/wayland/pool-buffer.h
@@ -17,18 +17,19 @@
 #include <wayland-client.h>
 
 struct pool_buffer {
-	struct wl_buffer *buffer;
-	cairo_surface_t *surface;
-	cairo_t *cairo;
-	PangoContext *pango;
-	uint32_t width, height;
-	void *data;
-	size_t size;
-	bool busy;
+        struct wl_buffer *buffer;
+        cairo_surface_t *surface;
+        cairo_t *cairo;
+        PangoContext *pango;
+        uint32_t width, height;
+        void *data;
+        size_t size;
+        bool busy;
 };
 
-struct pool_buffer *get_next_buffer(struct wl_shm *shm,
-	struct pool_buffer pool[static 2], uint32_t width, uint32_t height);
+struct pool_buffer *get_next_buffer(struct wl_shm *shm, struct pool_buffer pool[static 2],
+                                    uint32_t width, uint32_t height);
+
 void finish_buffer(struct pool_buffer *buffer);
 
 #endif
